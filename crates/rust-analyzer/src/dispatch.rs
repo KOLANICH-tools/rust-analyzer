6943c5b6dd85e91baec0a3f11a7a7bbc39e992f6
KOLANICH <kolan_n@mail.ru>
2022-05-30 19:44:15 +0300

    Revert upgrade of lsp_server to incompatible version.

--- crates/rust-analyzer/src/dispatch.rs
+++ crates/rust-analyzer/src/dispatch.rs
@@ -2,7 +2,6 @@
 use std::{fmt, panic, thread};
 
 use ide::Cancelled;
-use lsp_server::ExtractError;
 use serde::{de::DeserializeOwned, Serialize};
 
 use crate::{
@@ -238,10 +237,7 @@ impl<'a> NotificationDispatcher<'a> {
         };
         let params = match not.extract::<N::Params>(N::METHOD) {
             Ok(it) => it,
-            Err(ExtractError::JsonError { method, error }) => {
-                panic!("Invalid request\nMethod: {method}\n error: {error}",)
-            }
-            Err(ExtractError::MethodMismatch(not)) => {
+            Err(not) => {
                 self.not = Some(not);
                 return Ok(self);
             }
